b7631e494dd0b0b042da62d56755def785935711
ipCoelho <ipcoelho.personal@gmail.com>
2021-10-14 15:41:33 -0300

    footer and header sepated from the .html files.

---
+++ admin/dashboard/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div>
+        <span>Azonix Game Store&trade;</span>
+    </div>
+    <div class="copyright">
+        <span>&copy; Copyright 2021<br>Todos os direitos reservados - Política de Privacidade</span>
+    </div>
+    <div class="labels">
+        <span>Desenvolvido por Israel Pablo Coelho<br>versão 1.0.0</span>
+    </div>
+</footer>
---
+++ admin/dashboard/header.php
@@ -0,0 +1,40 @@
+<header>
+    <section id="header-conteiner">
+        <div id="title-conteiner">
+            <h1>CMS</h1>
+            <h3>Azonix Game Store&trade;<br>Gerenciamento de Conteúdo do Site</h3>
+        </div>
+        <div id="logo-conteiner">
+            <a href="../../index.html" target="_blank">
+                <img src="../img/logoIcon.png" alt="Azonix Game Store&copy;">
+            </a>
+        </div>
+    </section>
+    <section id="dashboard-conteiner">
+        <div id="category-conteiner">
+            <div class="category-item">
+                <img src="../img/gear.jpg" alt="Cadastrar">
+                <a href="register-games.php">Adm. de Jogos</a>
+            </div>
+            <div class="category-item">
+                <img src="../img/gear.jpg" alt="Cadastrar">
+                <a href="register-genre.php">Adm. de Gêneros</a>
+            </div>
+            <div class="category-item">
+                <img src="../img/gear.jpg" alt="Cadastrar">
+                <a href="register-image.php">Adm. de Imagens</a>
+            </div>
+            <div class="category-item">
+                <img src="../img/gear.jpg" alt="Cadastrar">
+                <a href="register-user.php">Usuários</a>
+            </div>
+        </div>
+        <div id="login-conteiner">
+            <h2>Bem vindo *usuario*</h2>
+            <img src="../img/home.jpg" alt="">
+            <a href="../login.php">
+                <button>Logout</button>
+            </a>
+        </div>
+    </section>
+</header>
